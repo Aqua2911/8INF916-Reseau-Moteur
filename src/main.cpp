97db58e2a0e676de1146ecc8067b9db9dd224f18
Aqua2911 <graveln@icloud.com>
2025-03-13 20:08:32 -0400

    Cubes and floor created
    Cube appearance bugged

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,48 @@
+#include <btBulletDynamicsCommon.h>
+#include <Corrade/Containers/GrowableArray.h>
+#include <Corrade/Containers/Optional.h>
+#include <Corrade/Containers/Pointer.h>
+#include <Magnum/Timeline.h>
+#include <Magnum/BulletIntegration/Integration.h>
+#include <Magnum/BulletIntegration/MotionState.h>
+#include <Magnum/BulletIntegration/DebugDraw.h>
 #include <Magnum/GL/DefaultFramebuffer.h>
+#include <Magnum/GL/Mesh.h>
 #include <Magnum/GL/Renderer.h>
+#include <Magnum/Math/Constants.h>
+#include <Magnum/Math/Color.h>
+#include <Magnum/Math/Time.h>
+#include <Magnum/MeshTools/Compile.h>
+#include <Magnum/MeshTools/Transform.h>
 #include <Magnum/Platform/GlfwApplication.h>
+#include <Magnum/Primitives/Cube.h>
+#include <Magnum/Primitives/UVSphere.h>
+#include <Magnum/SceneGraph/Camera.h>
+#include <Magnum/SceneGraph/Drawable.h>
 #include <Magnum/SceneGraph/MatrixTransformation3D.h>
 #include <Magnum/SceneGraph/Scene.h>
+#include <Magnum/Shaders/PhongGL.h>
 #include <Magnum/Trade/MeshData.h>
-#include <Magnum/Primitives/Cube.h>
-#include <Magnum/MeshTools/Compile.h>
-#include <Magnum/GL/Mesh.h>
 
-#include "Magnum/SceneGraph/Camera.h"
+#ifdef BT_USE_DOUBLE_PRECISION
+#error sorry, this example does not support Bullet with double precision enabled
+#endif
 
 using namespace Magnum;
 
-class MyApplication: public Platform::Application {
+using namespace Math::Literals;
+
+typedef SceneGraph::Object<SceneGraph::MatrixTransformation3D> Object3D;
+typedef SceneGraph::Scene<SceneGraph::MatrixTransformation3D> Scene3D;
+
+struct InstanceData {
+    Matrix4 transformationMatrix;
+    Matrix3x3 normalMatrix;
+    Color3 color;
+};
+
+class MyApplication: public Platform::GlfwApplication
+{
     public:
         explicit MyApplication(const Arguments& arguments);
 
@@ -21,46 +51,243 @@ class MyApplication: public Platform::Application {
         void keyPressEvent(KeyEvent& event) override;
         void pointerPressEvent(PointerEvent& event) override;
 
-        SceneGraph::Scene<SceneGraph::MatrixTransformation3D> _scene;
-        SceneGraph::Object<SceneGraph::MatrixTransformation3D> _cameraObject;
-        SceneGraph::Camera3D* _camera{};
+        GL::Mesh _box{NoCreate}, _sphere{NoCreate};
+        GL::Buffer _boxInstanceBuffer{NoCreate}, _sphereInstanceBuffer{NoCreate};
+        Shaders::PhongGL _shader{NoCreate};
 
-        GL::Mesh _cubeMesh;
-        std::vector<SceneGraph::Object<SceneGraph::MatrixTransformation3D>> _cubes;
+        Containers::Array<InstanceData> _boxInstanceData, _sphereInstanceData;
 
-        void setupScene();
+        btDbvtBroadphase _bBroadphase;
+        btDefaultCollisionConfiguration _bCollisionConfig;
+        btCollisionDispatcher _bDispatcher{&_bCollisionConfig};
+        btSequentialImpulseConstraintSolver _bSolver;
+
+        btDiscreteDynamicsWorld _bWorld{&_bDispatcher, &_bBroadphase, &_bSolver, &_bCollisionConfig};
+
+        Scene3D _scene;
+        SceneGraph::Camera3D* _camera;
+        SceneGraph::DrawableGroup3D _drawables;
+        Timeline _timeline;
+
+        Object3D *_cameraRig, *_cameraObject;
+
+        btBoxShape _bBoxShape{{0.5f, 0.5f, 0.5f}};
+        btSphereShape _bSphereShape{0.25f};
+        btBoxShape _bGroundShape{{4.0f, 0.5f, 4.0f}};
+
+        bool _drawCubes{true}, _shootBox{true};
+};
+
+class ColoredDrawable: public SceneGraph::Drawable3D {
+public:
+    explicit ColoredDrawable(Object3D& object, Containers::Array<InstanceData>& instanceData, const Color3& color, const Matrix4& primitiveTransformation, SceneGraph::DrawableGroup3D& drawables): SceneGraph::Drawable3D{object, &drawables}, _instanceData(instanceData), _color{color}, _primitiveTransformation{primitiveTransformation} {}
+
+private:
+    void draw(const Matrix4& transformation, SceneGraph::Camera3D&) override {
+        const Matrix4 t = transformation*_primitiveTransformation;
+        arrayAppend(_instanceData, InPlaceInit, t, t.normalMatrix(), _color);
+    }
+
+    Containers::Array<InstanceData>& _instanceData;
+    Color3 _color;
+    Matrix4 _primitiveTransformation;
+};
+
+class RigidBody: public Object3D {
+public:
+    RigidBody(Object3D* parent, Float mass, btCollisionShape* bShape, btDynamicsWorld& bWorld): Object3D{parent}, _bWorld(bWorld) {
+        /* Calculate inertia so the object reacts as it should with
+           rotation and everything */
+        btVector3 bInertia(0.0f, 0.0f, 0.0f);
+        if(!Math::TypeTraits<Float>::equals(mass, 0.0f))
+            bShape->calculateLocalInertia(mass, bInertia);
+
+        /* Bullet rigid body setup */
+        auto* motionState = new BulletIntegration::MotionState{*this};
+        _bRigidBody.emplace(btRigidBody::btRigidBodyConstructionInfo{
+            mass, &motionState->btMotionState(), bShape, bInertia});
+        _bRigidBody->forceActivationState(DISABLE_DEACTIVATION);
+        bWorld.addRigidBody(_bRigidBody.get());
+    }
+
+    ~RigidBody() {
+        _bWorld.removeRigidBody(_bRigidBody.get());
+    }
+
+    btRigidBody& rigidBody() { return *_bRigidBody; }
+
+    /* needed after changing the pose from Magnum side */
+    void syncPose() {
+        _bRigidBody->setWorldTransform(btTransform(transformationMatrix()));
+    }
+
+private:
+    btDynamicsWorld& _bWorld;
+    Containers::Pointer<btRigidBody> _bRigidBody;
 };
 
 MyApplication::MyApplication(const Arguments& arguments):
-    Platform::Application{arguments, Configuration{}.setTitle("Bullet Test")}
+    Platform::GlfwApplication(arguments, NoCreate)
 {
-    setupScene();
+    /* Try 8x MSAA, fall back to zero samples if not possible. Enable only 2x
+       MSAA if we have enough DPI. */
+    {
+        const Vector2 dpiScaling = this->dpiScaling({});
+        Configuration conf;
+        conf.setTitle("Bullet Test")
+            .setSize(conf.size(), dpiScaling);
+        GLConfiguration glConf;
+        glConf.setSampleCount(dpiScaling.max() < 2.0f ? 8 : 2);
+        if(!tryCreate(conf, glConf))
+            create(conf, glConf.setSampleCount(0));
+    }
+
+    /* Camera setup */
+    (*(_cameraRig = new Object3D{&_scene}))
+        .translate(Vector3::yAxis(3.0f))
+        .rotateY(40.0_degf);
+    (*(_cameraObject = new Object3D{_cameraRig}))
+        .translate(Vector3::zAxis(20.0f))
+        .rotateX(-25.0_degf);
+    (_camera = new SceneGraph::Camera3D(*_cameraObject))
+        ->setAspectRatioPolicy(SceneGraph::AspectRatioPolicy::Extend)
+        .setProjectionMatrix(Matrix4::perspectiveProjection(35.0_degf, 1.0f, 0.001f, 100.0f))
+        .setViewport(GL::defaultFramebuffer.viewport().size());
+
+    /* Create an instanced shader */
+    _shader = Shaders::PhongGL{Shaders::PhongGL::Configuration{}
+        .setFlags(Shaders::PhongGL::Flag::VertexColor|
+                  Shaders::PhongGL::Flag::InstancedTransformation)};
+    _shader.setAmbientColor(0x111111_rgbf)
+           .setSpecularColor(0x330000_rgbf)
+           .setLightPositions({{10.0f, 15.0f, 5.0f, 0.0f}});
+
+    /* Box and sphere mesh, with an (initially empty) instance buffer */
+    _box = MeshTools::compile(Primitives::cubeSolid());
+    _sphere = MeshTools::compile(Primitives::uvSphereSolid(16, 32));
+    _boxInstanceBuffer = GL::Buffer{};
+    _sphereInstanceBuffer = GL::Buffer{};
+    _box.addVertexBufferInstanced(_boxInstanceBuffer, 1, 0,
+        Shaders::PhongGL::TransformationMatrix{},
+        Shaders::PhongGL::NormalMatrix{},
+        Shaders::PhongGL::Color3{});
+    _sphere.addVertexBufferInstanced(_sphereInstanceBuffer, 1, 0,
+        Shaders::PhongGL::TransformationMatrix{},
+        Shaders::PhongGL::NormalMatrix{},
+        Shaders::PhongGL::Color3{});
+
+    /* Setup the renderer so we can draw the debug lines on top */
+    GL::Renderer::enable(GL::Renderer::Feature::DepthTest);
+    GL::Renderer::enable(GL::Renderer::Feature::FaceCulling);
+    GL::Renderer::enable(GL::Renderer::Feature::PolygonOffsetFill);
+    GL::Renderer::setPolygonOffset(2.0f, 0.5f);
+
+    /* Bullet setup */
+    //_debugDraw = BulletIntegration::DebugDraw{};
+    //_debugDraw.setMode(BulletIntegration::DebugDraw::Mode::DrawWireframe);
+    _bWorld.setGravity({0.0f, -10.0f, 0.0f});
+    //_bWorld.setDebugDrawer(&_debugDraw);
+
+    /* Create the ground */
+    auto* ground = new RigidBody{&_scene, 0.0f, &_bGroundShape, _bWorld};
+    new ColoredDrawable{*ground, _boxInstanceData, 0xffffff_rgbf,
+        Matrix4::scaling({4.0f, 0.5f, 4.0f}), _drawables};
+
+    /* Create boxes with random colors */
+    Deg hue = 42.0_degf;
+    for(Int i = 0; i != 5; ++i) {
+        for(Int j = 0; j != 5; ++j) {
+            for(Int k = 0; k != 5; ++k) {
+                auto* o = new RigidBody{&_scene, 1.0f, &_bBoxShape, _bWorld};
+                o->translate({i - 2.0f, j + 4.0f, k - 2.0f});
+                o->syncPose();
+                new ColoredDrawable{*o, _boxInstanceData,
+                    Color3::fromHsv({hue += 137.5_degf, 0.75f, 0.9f}),
+                    Matrix4::scaling(Vector3{0.5f}), _drawables};
+            }
+        }
+    }
+
+    /* Loop at 60 Hz max */
+    setSwapInterval(1);
+    setMinimalLoopPeriod(16.0_msec);
+    _timeline.start();
+
 }
 
 void MyApplication::drawEvent() {
-    GL::defaultFramebuffer.clear(GL::FramebufferClear::Color | GL::FramebufferClear::Depth);
+    GL::defaultFramebuffer.clear(GL::FramebufferClear::Color|GL::FramebufferClear::Depth);
 
+    /* Housekeeping: remove any objects which are far away from the origin */
+    for(Object3D* obj = _scene.children().first(); obj; )
+    {
+        Object3D* next = obj->nextSibling();
+        if(obj->transformation().translation().dot() > 100*100)
+            delete obj;
 
-    for (auto& cube : _cubes) {
-        // Rendu des cubes (ici, un simple rendu de debug, il faudra un shader plus tard)
-        //_cubeMesh.draw();
+        obj = next;
     }
 
+    /* Step bullet simulation */
+    _bWorld.stepSimulation(_timeline.previousFrameDuration(), 5);
+
+    if(_drawCubes) {
+        /* Populate instance data with transformations and colors */
+        arrayResize(_boxInstanceData, 0);
+        arrayResize(_sphereInstanceData, 0);
+        _camera->draw(_drawables);
+
+        _shader.setProjectionMatrix(_camera->projectionMatrix());
+
+        /* Upload instance data to the GPU (orphaning the previous buffer
+           contents) and draw all cubes in one call, and all spheres (if any)
+           in another call */
+        _boxInstanceBuffer.setData(_boxInstanceData, GL::BufferUsage::DynamicDraw);
+        _box.setInstanceCount(_boxInstanceData.size());
+        _shader.draw(_box);
+
+        _sphereInstanceBuffer.setData(_sphereInstanceData, GL::BufferUsage::DynamicDraw);
+        _sphere.setInstanceCount(_sphereInstanceData.size());
+        _shader.draw(_sphere);
+    }
+
+    /* Debug draw. If drawing on top of cubes, avoid flickering by setting
+       depth function to <= instead of just <.
+    if(_drawDebug) {
+        if(_drawCubes)
+            GL::Renderer::setDepthFunction(GL::Renderer::DepthFunction::LessOrEqual);
+
+        _debugDraw.setTransformationProjectionMatrix(
+            _camera->projectionMatrix()*_camera->cameraMatrix());
+        _bWorld.debugDrawWorld();
+
+        if(_drawCubes)
+            GL::Renderer::setDepthFunction(GL::Renderer::DepthFunction::Less);
+    }
+    */
+
     swapBuffers();
+    _timeline.nextFrame();
     redraw();
 }
 
 
-void MyApplication::keyPressEvent(KeyEvent &event) {
-    // Déplacement de la caméra avec WASD/QE
-    if (event.key() == Key::W) _cameraObject.translate(Vector3::zAxis(-0.1f));
-    if (event.key() == Key::S) _cameraObject.translate(Vector3::zAxis(0.1f));
-    if (event.key() == Key::A) _cameraObject.translate(Vector3::xAxis(-0.1f));
-    if (event.key() == Key::D) _cameraObject.translate(Vector3::xAxis(0.1f));
-    if (event.key() == Key::Q) _cameraObject.translate(Vector3::yAxis(-0.1f));
-    if (event.key() == Key::E) _cameraObject.translate(Vector3::yAxis(0.1f));
+void MyApplication::keyPressEvent(KeyEvent& event) {
+    /* Movement */
+    if(event.key() == Key::Down) {
+        _cameraObject->rotateX(5.0_degf);
+    } else if(event.key() == Key::Up) {
+        _cameraObject->rotateX(-5.0_degf);
+    } else if(event.key() == Key::Left) {
+        _cameraRig->rotateY(-5.0_degf);
+    } else if(event.key() == Key::Right) {
+        _cameraRig->rotateY(5.0_degf);
+        /* What to shoot */
+    } else if(event.key() == Key::S) {
+        _shootBox ^= true;
+    } else return;
 
-    redraw();
+    event.setAccepted();
 }
 
 void MyApplication::pointerPressEvent(PointerEvent& event) {
@@ -68,35 +295,6 @@ void MyApplication::pointerPressEvent(PointerEvent& event) {
         Debug{} << "Click détecté ! Tir d'une sphère à implémenter...";
         // TODO: Ajouter la gestion de la sphère qui est tirée
     }
-}
-
-
-void MyApplication::setupScene() {
-    // Charger le maillage du cube
-    _cubeMesh = MeshTools::compile(Primitives::cubeSolid());
-
-    // Placer le sol (un grand cube aplati)
-    auto& floor = _scene.addChild<SceneGraph::Object<SceneGraph::MatrixTransformation3D>>();
-    floor.translate(Vector3{0.0f, -0.5f, 0.0f}).scale(Vector3{10.0f, 0.1f, 10.0f});
-
-
-    // Générer une grille de cubes 10x10
-    for (int x = 0; x < 10; ++x) {
-        for (int z = 0; z < 10; ++z) {
-            auto& cube = _scene.addChild<SceneGraph::Object<SceneGraph::MatrixTransformation3D>>();
-            cube.translate(Vector3{float(x) - 4.5f, 0.0f, float(z) - 4.5f});
-            //_cubes.push_back(std::move(cube));
-        }
-    }
-
-
-    // Configurer la caméra
-    _cameraObject.translate(Vector3{0.0f, 2.0f, 10.0f});
-    _camera = new SceneGraph::Camera3D{_cameraObject};
-    _camera->setProjectionMatrix(Matrix4::perspectiveProjection(
-    Rad{Deg{45.0f}}, // Conversion explicite
-    1.0f, 0.1f, 100.0f
-));
 }
 
 MAGNUM_APPLICATION_MAIN(MyApplication)
